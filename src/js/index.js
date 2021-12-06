7eb5a364f3d554b06cf499de9da4aad72f5925d3
Łukasz Szwed <lszwedxx@gmail.com>
2021-12-06 18:49:08 +0100

    change primary secondary color while spin circle

--- src/js/index.js
+++ src/js/index.js
@@ -3,18 +3,33 @@ const navBtn = document.querySelector('.nav__btn');
 const navList = document.querySelector('.nav__list');
 //carousel
 const carousel = document.querySelector('.carousel');
+const circle = document.querySelector('.circle');
 const rightBtn = document.querySelector('.change-btn--right');
 const leftBtn = document.querySelector('.change-btn--left');
 let mainImg = document.querySelectorAll('.main-content__img > img');
+const order = document.querySelector('.order')
+const vegeArray = [carousel,circle,rightBtn,leftBtn,order]
 let rotateVal = 0;
 let imgNum=6;
 //carousel btn
+const checkColor = (color)=>{
+    if(color==2){
+        vegeArray.forEach(item =>{
+            item.classList.add('vege')
+        })
+    }else{
+        vegeArray.forEach(item =>{
+            item.classList.remove('vege')
+        })
+    }
+}
 const showMainImg = ()=>{
     [...mainImg].forEach(image=>{
         image.classList.remove('show');
     })
     let img = [...mainImg].filter(image=>image.dataset.index==imgNum);
     img[0].classList.add('show');
+    checkColor(img[0].dataset.background);
 }
 const rotate = (direction) =>{
     if(direction=='right'){
